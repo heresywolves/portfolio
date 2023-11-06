3e120b231d0c478a51861a9cb7b1de1672b4f497
Howl Kimura <howlkimuradev@gmail.com>
2023-11-06 17:24:05 +0300

    Update profile pic

--- src/loadImages.js
+++ src/loadImages.js
@@ -1,6 +1,7 @@
 import Me from './img/me.jpg';
+import Me2 from './img/me2.png';
 
 export const loadImages = (() => {
   const myImg = document.querySelector('.my-photo');
-  myImg.src = Me;
+  myImg.src = Me2;
 })();
