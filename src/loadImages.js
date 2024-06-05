eb9ddfc3faf92ed10e84b95b1722b82ed1293772
Howl Kimura <howlkimuradev@gmail.com>
2024-06-05 19:57:01 +0300

    style projects

--- src/loadImages.js
+++ src/loadImages.js
@@ -1,10 +1,11 @@
 import Me from './img/me.jpg';
 import Me2 from './img/me2.png';
+import Me3 from './img/me3.png';
 import phoneMenu from './img/phone-menu.png';
 
 export const loadImages = (() => {
   const myImg = document.querySelector('.my-photo');
-  myImg.src = Me2;
+  myImg.src = Me3;
   const menuPng = document.querySelector('.mobile-menu');
   menuPng.src = phoneMenu;
 })();
--- src/projects.js
+++ src/projects.js
@@ -11,11 +11,11 @@ import restaurantPC from './img/restaurant-pc.png'
 export let Projects = (() => {
   let projectsObj = {
     dashboard: {
-      name: 'Admin Dashboard',
+      name: 'Панель администратора',
       pc: dashboardPC,
       phone: dashboardPhone,
-      description: 'This dashboard serves as a responsive layout example that utilizes the strengths of grid and flexbox layouts.',
-      stack: 'Vanilla JS, HTML, CSS',
+      description: 'Страница панели администратора в современном стиле, которая демонстрирует всю прелесть адаптивной grid и flex верстки.',
+      stack: 'JS, HTML, CSS',
       github: 'https://github.com/howlbones/admin-dashboard',
       live: 'https://howlbones.github.io/admin-dashboard/'
     },
@@ -67,9 +67,65 @@ export let Projects = (() => {
   }
 
   function load() {
-
+    const container = document.querySelector('.projects-container');
     for (const key in projectsObj) {
 
+      const itemContainer = document.createElement('div');
+      itemContainer.classList.add('project-container');
+
+      const txtContainer = document.createElement('div');
+      txtContainer.classList.add('text-container');
+
+      const imgContainer = document.createElement('a');
+      imgContainer.classList.add('img-container');
+      imgContainer.href = projectsObj[key].live;
+      imgContainer.setAttribute("target", "_blank");
+
+      const nameEl = document.createElement('p');
+      nameEl.classList.add('project-name');
+      nameEl.textContent = projectsObj[key].name;
+      txtContainer.appendChild(nameEl);
+
+      const description = document.createElement('p');
+      description.classList.add('project-description');
+      description.textContent = projectsObj[key].description;
+      txtContainer.appendChild(description);
+      
+      const stack = document.createElement('p');
+      stack.classList.add('project-stack');
+      stack.textContent = projectsObj[key].stack;
+      txtContainer.appendChild(stack);
+
+      const pcImg = document.createElement('img');
+      pcImg.classList.add('project-pc-img');
+      pcImg.src = projectsObj[key].pc;
+      pcImg.href = projectsObj[key].live;
+      imgContainer.appendChild(pcImg);
+
+      if (projectsObj[key].phone != '') {
+        const phoneImg = document.createElement('img');
+        phoneImg.classList.add('project-phone-img');
+        phoneImg.src = projectsObj[key].phone;
+        imgContainer.appendChild(phoneImg);
+      }
+
+      const gitLink = document.createElement('a');
+      gitLink.classList.add('project-git-link');
+      gitLink.href = projectsObj[key].github;
+      gitLink.textContent = "Github";
+      gitLink.setAttribute("target", "_blank");
+      txtContainer.appendChild(gitLink);
+
+      const liveLink = document.createElement('a');
+      liveLink.classList.add('project-live-link');
+      liveLink.href = projectsObj[key].live;
+      liveLink.textContent = "Публикация";
+      liveLink.setAttribute("target", "_blank");
+      txtContainer.appendChild(liveLink);
+
+      itemContainer.appendChild(imgContainer);
+      itemContainer.appendChild(txtContainer);
+      container.appendChild(itemContainer);
     }
   }
 
