9fa65e391831e7fa756b467bb7b22baa5eac5647
Howl Kimura <howlkimuradev@gmail.com>
2024-06-05 16:07:15 +0300

    add tech stack skills

--- src/techStack.js
+++ src/techStack.js
@@ -8,8 +8,67 @@ import threeImg from './img/three.png'
 import babelImg from './img/babel.png'
 import reactImg from './img/react.png'
 import sassImg from './img/sass.png'
+import vueImg from './img/vue.png'
 
 function Init (doc) {
+  const skills = [
+    {
+      text: "JavaScript (ES5+)",
+      image: javascriptImg
+    },
+    {
+      text: "NPM",
+      image: npmImg
+    },
+    {
+      text: "Webpack",
+      image: webpackImg
+    },
+    {
+      text: "Jest",
+      image: jestImg
+    },
+    {
+      text: "React (+Redux)",
+      image: reactImg
+    },
+    {
+      text: "Vue",
+      image: vueImg
+    },
+    {
+      text: "Babel",
+      image: babelImg
+    },
+    {
+      text: "Node",
+      image: nodeImg
+    },
+    {
+      text: "ThreeJS",
+      image: threeImg
+    },
+    {
+      text: "Git",
+      image: githubImg
+    }
+  ]
+
+  skills.forEach((item) => {
+    const container = document.createElement('div');
+    if (item.image) {
+      const img = document.createElement('img');
+      img.classList.add('skill-img');
+      img.src = item.image;
+      container.appendChild(img);
+    }
+    const text = document.createElement('p');
+    text.classList.add('skill-text');
+    text.textContent = item.text;
+    container.appendChild(text);
+    container.classList.add('skill-item-container');
+    doc.appendChild(container);
+  })
 
 }
 
